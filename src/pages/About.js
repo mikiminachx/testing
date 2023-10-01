d7fda6620893d999f1131176aa64d58f2ccb382b
mekolau <dezmeko@icloud.com>
2023-10-01 16:13:33 +0800

    hi

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
   const [markdown, setMarkdown] = useState('');
 
   useEffect(() => {
-    import('../data/about.md')
+    import('src/data/about.md')
       .then((res) => {
         fetch(res.default)
           .then((r) => r.text())
